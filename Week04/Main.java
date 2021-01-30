216420362d0e2e155f372075e505e022e86aaaa9
Jair Valadez <val20013@byui.edu>
2021-01-30 11:48:29 -0600

    Updated Main.java

--- Week04/Main.java
+++ Week04/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Jaden");
-	System.out.println("Hello Jaden, my name is Karly");   
+	System.out.println("Hello Jaden, my name is Karly");
+	System.out.println("Hello Jaden, my name is Jair");
     }
 }
